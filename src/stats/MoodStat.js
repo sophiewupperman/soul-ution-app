bf81531aca412abf2c2c6666a862cf3c66d6c7a4
sophiewupperman <sophie@einfluss-consulting.com>
2019-05-22 13:07:17 +0200

    Experiment Line Graph

--- src/stats/MoodStat.js
+++ src/stats/MoodStat.js
@@ -9,43 +9,41 @@ const StyledMoodStat = styled.section`
 `
 const data = [
   {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
+    uv: 100,
+    pv: 1000000,
+    amt: 1000000,
   },
   {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
+    uv: 0,
+    pv: 0,
+    amt: 0,
   },
   {
-    name: 'Page C',
     uv: 2000,
     pv: 9800,
     amt: 2290,
   },
   {
-    name: 'Page D',
     uv: 2780,
     pv: 3908,
     amt: 2000,
   },
   {
-    name: 'Page E',
     uv: 1890,
     pv: 4800,
     amt: 2181,
   },
   {
-    name: 'Page F',
     uv: 2390,
     pv: 3800,
     amt: 2500,
   },
   {
-    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
     uv: 3490,
     pv: 4300,
     amt: 2100,
@@ -58,15 +56,11 @@ export default function MoodStat() {
       <h2>MOOD</h2>
       <LineChart
         width={400}
-        height={400}
+        height={150}
         data={data}
         margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
       >
-        <XAxis dataKey="name" />
-        <Tooltip />
-        <CartesianGrid stroke="#f5f5f5" />
         <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
-        <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
       </LineChart>
     </StyledMoodStat>
   )
