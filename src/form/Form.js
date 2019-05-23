434da7371bf1f7d98639a5943b911465a4f67390
sophiewupperman <sophie@einfluss-consulting.com>
2019-05-23 11:08:54 +0200

    Toggle habit buttons

--- src/form/Form.js
+++ src/form/Form.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import MoodSlider from './MoodSlider'
-import HabitButtons from './HabitButtons'
+import HabitButton from './HabitButton'
 
 const StyledForm = styled.body`
   display: grid;
@@ -11,11 +11,39 @@ const StyledForm = styled.body`
 `
 
 export default function Stats() {
+  const [habits, setHabits] = useState([
+    { name: 'EXERCISE', isChoosen: false },
+    { name: 'SLEEP', isChoosen: false },
+    { name: 'VITAMIN D', isChoosen: false },
+    { name: 'JOURNAL', isChoosen: false },
+    { name: 'SOCIAL', isChoosen: false },
+    { name: 'NOURISHMENT', isChoosen: false },
+  ])
+
+  const toggleHabbitChoosen = index => {
+    setHabits([
+      ...habits.slice(0, index),
+      { ...habits[index], isChoosen: !habits[index].isChoosen },
+      ...habits.slice(index + 1),
+    ])
+
+    console.log(habits)
+    // Save in LocalStorage
+  }
+
   return (
     <StyledForm>
       <h2>SOUL OUTION</h2>
       <MoodSlider />
-      <HabitButtons />
+      {habits.map((habit, index) => {
+        return (
+          <HabitButton
+            habit={habit}
+            index={index}
+            toggleHabbitChoosen={toggleHabbitChoosen}
+          />
+        )
+      })}
     </StyledForm>
   )
 }
---
+++ src/form/HabitButton.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+const StyledButttonForm = styled.div`
+  display: grid;
+  justify-items: center;
+`
+
+const StyledHabitButton = styled.button`
+  background-color: ${props => (props.active ? ' white' : '#889ab5')};
+  border: none;
+  border-radius: 20px;
+  color: ${props => (props.active ? '#889ab5' : 'white')};
+  width: 200px;
+  height: 30px;
+  margin: 20px;
+  text-align: center;
+  text-decoration: none;
+  font-size: 20px;
+`
+
+const HabitButton = ({ habit, toggleHabbitChoosen, index }) => {
+  const [active, setActive] = useState(false)
+
+  const onHabbitButtonCLick = () => {
+    setActive(!active)
+    toggleHabbitChoosen(index)
+  }
+
+  return (
+    <StyledButttonForm>
+      <label>Maintained your habits?</label>
+      <StyledButttonForm>
+        <StyledHabitButton active={active} onClick={onHabbitButtonCLick}>
+          {habit.name}
+        </StyledHabitButton>
+      </StyledButttonForm>
+    </StyledButttonForm>
+  )
+}
+
+export default HabitButton
